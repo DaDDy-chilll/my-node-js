101c11c9ee47c57f186d35840e9f37639ea6dadb
DaDDy <blacklistuser420@gmail.com>
2022-06-11 22:42:27 +0630

    my node js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const https = require("https");
+const url = require('url');
 const bdparser = require("body-parser");
+const mongoose = require('mongoose');
 const res = require("express/lib/response");
 const { json } = require("express/lib/response");
 const { log } = require("console");
@@ -14,6 +16,44 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+//! Start Databse
+
+mongoose.connect('mongodb://localhost:27017/mytodo');
+
+const itemSchema = mongoose.Schema({
+  name:String
+});
+
+const routeSchema = mongoose.Schema({
+  name:String,
+  item:[itemSchema]
+})
+
+const Items = mongoose.model('Items',itemSchema);
+const Routes = mongoose.model('Routes',routeSchema);
+
+const item1 = new Items({
+  name:'Apple'
+});
+const item2 = new Items({
+  name:'Orange'
+});
+const item3 = new Items({
+  name:'starfruit'
+});
+const item4 = new Items({
+  name:'piapple'
+});
+const item5 = new Items({
+  name:'mango'
+});
+const item6 = new Items({
+  name:'kiwi'
+});
+
+
+
+//! End Database
 
 //! Start Calculator Route
 app.get("/calculator", (req, res) => {
@@ -107,9 +147,60 @@ app.post('/newsletter',(req,res)=>{
 });
 //! End Newsletter Route
 ////-------------------------------------------------------
+//! Start To DO List Route
+
+const defaultItem = [item1,item2,item3,item4,item5,item6];
+
 app.get("/todolist", (req, res) => {
-  res.sendFile(__dirname + "/routefile/todolist.html");
+  Items.find({},(err,founditem)=>{
+    if(founditem.length === 0){
+      Items.insertMany(defaultItem,(err)=>{
+        if(err){
+          console.log(err);
+        }else{
+          console.log('Successfully Inserted');
+        }
+      });
+      res.redirect('/todolist');
+    }else{
+      res.render('todolist/todolist',{listTitle:'ToDay',listItem:founditem});
+    }
+  });
 });
+app.post('/todolist',(req,res)=>{
+  const newItem = req.body.userdata;
+  const listTitle = req.body.userTitle;
+  const useItem = new Items({
+    name:newItem
+  });
+  useItem.save();
+  res.redirect('/todolist');
+});
+
+app.get(`/todolist/?:userRoute`,(req,res)=>{
+  console.log('hi');
+  // const subRoute = req.params.;
+  // console.log(subRoute);
+  // Routes.findOne({name:Params},(err,foundlist)=>{
+  //   if(!err){
+  //     if(!foundlist){
+  //       const list = new Routes({
+  //         name:Params,
+  //         item:defaultItem
+  //       });
+  //       list.save();
+  //       console.log(foundlist.name,foundlist.item);
+  //       res.redirect('/'+Params);
+  //     }else{
+  //       console.log(foundlist.name,foundlist.item);
+  //       res.render('todolist/todolist.ejs',{listTitle:foundlist.name,listItem:foundlist.item});
+  //     }
+  //   }
+  // });
+});
+
+//! End To DO List Route
+////-------------------------------------------------------
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server is running on ${PORT}`);
